e70eee1e555c1377e1cc8cf38649741b1e310c2d
Andrew Xia <axia@mit.edu>
2016-05-15 17:08:37 -0400

    starting reactJS

---
+++ Do not upload/script.js
@@ -0,0 +1,11 @@
+var huding = new Date();
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + huding.getFullYear() + " " + huding.getHours() + " Huding Enterprises. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
