9a3723e7a3e4a41de35ada0eecb7e8b95cb6f87a
andrewqandrew <axia@mit.edu>
2016-10-08 21:40:16 -0400

    final version, added prefix search

--- meteor/main.js
+++ meteor/main.js
@@ -16,10 +16,7 @@ app.controller('mainCtrl', function($scope,$http) {
         ];
 
     $scope.t9Filter = function(search){
-        // return $scope.search;
         if (search != null && search != ""){
-            words = [];
-            
             // first, get deep into the tree
             currDict = $scope.dict;
             for (i = 0; i < search.length; i++){
@@ -33,24 +30,56 @@ app.controller('mainCtrl', function($scope,$http) {
                 else
                     currDict = null;
                 }
-                // words.push(ltr);
             }
-
             // entered a node with no results
             if (currDict == null){
-                return "No Results"
+                return null;
             }
-            if (currDict['word'].length != 0)
-                return currDict['word'];
-            return "No Results"
-            // words.push(currDict);
-
-            // return search.length
-            return words;
+            if (currDict['word'].length == 0)
+                return null;
+            return currDict['word'];
         }
         else 
-            return ""
-    }
+            return null;
+    };
+
+    $scope.t9Prefix = function(search){
+        if (search != null && search != ""){
+            // first, get deep into the tree
+            currDict = $scope.dict;
+            for (i = 0; i < search.length; i++){
+                ltr = search[i]
+                if (currDict != null){
+                    if (currDict.hasOwnProperty(ltr)){
+                    // if (currDict[ltr]['word'].length != 0)
+                    //     words.push(currDict[ltr]['word'])
+                    currDict = currDict[ltr];
+                }
+                else
+                    currDict = null;
+                }
+            }
+            // entered a node with no results
+            if (currDict == null){
+                return null;
+            }
+            // if (currDict['word'].length == 0)
+            //     return null;
+            // find nearest 10 prefixes?
+            word = [];
+            for (var key in currDict){
+                if (key != 'word'){
+                    if (currDict[key]['word'].length != 0)
+                        for (var w in currDict[key]['word'])
+                            word.push(currDict[key]['word'][w]);
+                }
+                // word.push(key);
+            }
+            return word;
+        }
+        else 
+            return null;
+    };
 
     // http://stackoverflow.com/questions/13020821/how-to-load-json-into-my-angular-js-ng-model 
     $http.get('http://qandrew.github.io/meteor/result.json')
